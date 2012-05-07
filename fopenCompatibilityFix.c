9c72853b7942c5a454b8de5f53800023b38cf22d
Tito Ciuro <tciuro@mac.com>
2012-05-06 22:40:26 -0700

    Fixed #26: Message 'fopen$UNIX2003 called from function llvm_gcda_start_file' shown in console

---
+++ fopenCompatibilityFix.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+FILE *fopen$UNIX2003( const char *filename, const char *mode )
+{
+    return fopen(filename, mode);
+}
+
+size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d )
+{
+    return fwrite(a, b, c, d);
+}
